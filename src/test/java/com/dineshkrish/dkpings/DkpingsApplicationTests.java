3d9557f2a4567cda3b75b1b0ac08e379a71709e5
idineshkrishnan <dinesh@dineshkrish.com>
2019-07-17 17:14:14 -0700

    initial commit

---
+++ src/test/java/com/dineshkrish/dkpings/DkpingsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dineshkrish.dkpings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DkpingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
